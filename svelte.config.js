e9c09c46f6f9fe00e393764b0c7a42068508bdaa
MoonHo <75512752+monho@users.noreply.github.com>
2025-08-07 11:34:17 +0900

    Refactor svelte.config.js to streamline export and remove unnecessary comments

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,14 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter({
-			runtime: 'nodejs18.x' // Vercel 런타임 명시
-		}),
-		alias: {
-			$db: './src/db'
-		}
-	},
-	preprocess: vitePreprocess()
+export default {
+  kit: {
+    adapter: adapter({
+      runtime: 'nodejs18.x'
+    }),
+    alias: {
+      $db: './src/db'
+    }
+  },
+  preprocess: vitePreprocess()
 };
-
-export default config;

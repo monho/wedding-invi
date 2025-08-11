fe78fb3441c32fb230cd288e8c7725e394eda291
MoonHo <75512752+monho@users.noreply.github.com>
2025-08-11 11:57:58 +0900

    chore: update @sveltejs/adapter-auto to version 2.1.1 and caniuse-lite to version 1.0.30001734

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.Cby8MUGo.js","_app/immutable/chunks/yzEkgqGX.js","_app/immutable/chunks/IHki7fMi.js","_app/immutable/chunks/2_z933pP.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/static/_app/immutable/entry/start.BAB-Gv74.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/2_z933pP.js";export{o as load_css,r as start};
